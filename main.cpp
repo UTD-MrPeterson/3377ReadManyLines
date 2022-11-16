2fdcd1f852a438510167707179b138e7665c60c7
Erik Peterson <erik@thebibleway.com>
2022-11-16 14:44:21 -0600

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+
+void readCWay() {
+    long lineCount = 0;
+
+    char inputLine[100];
+    int character;
+
+    while ( true) {
+        gets(inputLine);
+        //printf("%s\n", inputLine);
+        lineCount++;
+        if ((character = getc(stdin))==EOF) {
+            break;
+        }
+        ungetc(character, stdin);
+    }
+    printf("Line count is %ld\n", lineCount);
+}
+
+void readCPlusPlusWay() {
+    // From video https://www.youtube.com/watch?v=MHBG_R_dhwk
+    std::string inputLine;
+    long lineCount = 0;
+
+    while(true) {
+        std::getline(std::cin, inputLine);
+        lineCount++;
+        if ( std::cin.eof()) {
+            break;
+        }
+    }
+    std::cout << "Line count is " << lineCount << std::endl;
+}
+
+int main(int argc, char** argv) {
+    if ( argc < 2) {
+        std::cerr << "Need a type (C or C++)";
+    }
+
+    if ( strcmp(argv[1], "C") == 0) {
+        readCWay();
+    } else {
+        readCPlusPlusWay();
+    }
+
+    /*std::ofstream outputFile("input");
+
+    for ( int i=0; i<999'999; i++) {
+        outputFile << "Hello, world!\n";
+    }
+    outputFile << "Hello, world!";
+    outputFile.close();*/
+    return 0;
+}
